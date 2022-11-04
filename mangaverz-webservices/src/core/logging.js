0458dff9f101b9ea197f15ac7e0e42bb33e3f259
Berat <berat.kamali@student.hogent.be>
2022-11-04 20:26:48 +0100

    koa en winston1

---
+++ mangaverz-webservices/src/core/logging.js
@@ -0,0 +1,17 @@
+const winston = require('winston');
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(),
+  defaultMeta: {
+    service: 'user-service'
+  },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.Console()
+  ],
+});
+
+module.exports.getLogger = () => logger;
--- mangaverz-webservices/src/index.js
+++ mangaverz-webservices/src/index.js
@@ -1,9 +1,17 @@
-const Koa = require('koa')
+//requirements en imports
+const Koa = require('koa');
+const {
+  getLogger
+} = require('./core/logging.js');
+
+
+//object instanties en aanroep methodes
 const app = new Koa();
+const logger = getLogger();
 
 app.use(async (ctx, next) => {
-  ctx.body = 'Hello World';
-  await next();
-})
-string = ''
+  //ctx.body = 'Hello World';
+  ctx.body = "Hellowww";
+});
 app.listen(9000);
+logger.info("Server start phieewww");
