7bd296b36bea9065e646d56ad37e5095e6a39a99
Berat <berat.kamali@student.hogent.be>
2022-11-04 21:13:24 +0100

    configuratie

--- mangaverz-webservices/src/index.js
+++ mangaverz-webservices/src/index.js
@@ -3,12 +3,18 @@ const Koa = require('koa');
 const {
   getLogger
 } = require('./core/logging.js');
+const config = require('config');
 
 
-//object instanties en aanroep methodes
+//object instanties, aanroep methodes, variabelen
 const app = new Koa();
 const logger = getLogger();
 
+const NODE_ENV = config.get('env')
+const LOG_LEVEL = config.get('log.level');
+const LOG_DISABLED = config.get('log.disabled');
+
+logger.info(`${NODE_ENV} level: ${LOG_LEVEL}, disabled: ${LOG_DISABLED}`);
 app.use(async (ctx, next) => {
   //ctx.body = 'Hello World';
   ctx.body = "Hellowww";
